5f96218b671528cc33f3129260539822fa0cf86f
Nick Espig <nickespig@gmail.com>
2019-08-14 21:14:21 +0200

    Load answers from answers.txt

--- bot/game/main.py
+++ bot/game/main.py
@@ -2,12 +2,12 @@ import base64
 import json
 import re
 import sys
+from contextlib import closing
 from datetime import datetime
 from http.cookiejar import MozillaCookieJar
 from os import getcwd
 from pathlib import Path
 from typing import Dict, List
-from urllib.error import URLError
 from urllib.parse import urlencode, urlunsplit
 from urllib.request import HTTPCookieProcessor, Request, build_opener
 
@@ -98,8 +98,46 @@ class EnergySession:
 
 
 class Operator:
-    def __init__(self, session: EnergySession):
+    def __init__(self, session: EnergySession, answers: Dict = None, answer_file: str or Path = None,
+                 guess_randomly: bool = False):
         self.session = session
+        self.guess_randomly = guess_randomly
+        if answers and answer_file:
+            raise AttributeError('"answers" and "answer_file" are mutually exclusive.')
+
+        self.answers = answers or {}
+        if not self.answers:
+            self.load_answers(answer_file)
+
+    def load_answers(self, answer_file: str or Path = None):
+        path = (Path(answer_file) if answer_file else Path() / 'answers.txt').absolute()
+        str_path = str(path)
+        with closing(path.open()) as file:
+            current_question = None
+            for line_nr, line in enumerate(file.readlines(), 1):
+                line = line.strip().lower()
+                error_line = (str_path, line_nr, None, None)
+                if line.startswith('question'):
+                    ids = re.findall('\[(\d+)\]', line)
+                    if not ids:
+                        raise SyntaxError('Question ID missing', error_line)
+                    elif int(ids[0]) in self.answers:
+                        raise SyntaxError(f'Duplicate Question', error_line)
+                    current_question = int(ids[0])
+                elif line.startswith('answer'):
+                    ids = re.findall('(\d+)', line)
+                    if not ids:
+                        if not self.guess_randomly:
+                            raise SyntaxError(f'Answer missing', error_line)
+                        answer = None
+                    elif not current_question:
+                        raise SyntaxError(f'Answer found but no connected question for it', error_line)
+                    elif int(ids[0]) not in [1, 2, 3]:
+                        raise SyntaxError(f'Answer must be either 1, 2 or 3', error_line)
+                    else:
+                        answer = int(ids[0])
+                    self.answers[current_question] = answer
+                    current_question = None
 
     @property
     def logged_in(self):
@@ -112,7 +150,7 @@ class Operator:
 class Menu:
     def __init__(self):
         self.session = EnergySession()
-        self.operator = Operator(self.session)
+        self.operator = Operator(self.session, guess_randomly=True)
         self.running = True
 
         self.tel = ''
